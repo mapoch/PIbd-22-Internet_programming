3057084b3cb10de5f2fed90292edfb37c9985167
Дмитрий Александров <mapoch2002@yandex.ru>
2021-11-26 20:55:19 +0400

    лаба 6

---
+++ src/Counter.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useState } from 'react';
+import Button from './Button';
+
+interface CounterProps {
+	start: number;
+	step: number;
+};
+
+var Counter = (props: CounterProps) => {
+	var [ count, setCount ] = useState<number>(props.start);
+	
+	var handleIncrease = () => {
+		setCount(count + props.step);
+	};
+	
+    return (
+		<div>
+			<div>Значение счётчика: {count}.</div>
+			<Button text={"Прибавить на " + props.step} onClick={handleIncrease} />
+		</div>
+    );
+};
+export default Counter;
